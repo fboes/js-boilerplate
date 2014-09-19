c4ee8df1c6c921a67d55718c66b0daf9664f3bdc
Frank Boës <frank.boes@shift.agency>
2014-09-19 17:27:58 +0200

    Enabling live reload

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,55 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    dirs: {
-      web: ''
-    },
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		dirs: {
+			web: ''
+		},
 
-    jslint: {
-      build: {
-        src: [
-          '<%= dirs.web %>js/*.js',
-          '!<%= dirs.web %>js/*.min.js'
-        ],
-        directives: {
-          browser: true, white: true
-        },
-        options: {
-          failOnError: false
-        }
-      }
-    },
+		jslint: {
+			build: {
+				src: [
+					'<%= dirs.web %>js/*.js',
+					'!<%= dirs.web %>js/*.min.js'
+				],
+				directives: {
+					browser: true, white: true
+				},
+				options: {
+					failOnError: false
+				}
+			}
+		},
 
-    uglify: {
-      build: {
-        files: {
-          '<%= dirs.web %>js/main.min.js': ['<%= dirs.web %>js/main.js', '!<%= dirs.web %>js/main.min.js'],
-          '<%= dirs.web %>js/jquery-example.min.js': ['<%= dirs.web %>js/jquery-example.js', '!<%= dirs.web %>js/jquery-example.min.js']
-        }
-      }
-    },
+		uglify: {
+			build: {
+				files: {
+					'<%= dirs.web %>js/main.min.js': ['<%= dirs.web %>js/main.js', '!<%= dirs.web %>js/main.min.js'],
+					'<%= dirs.web %>js/jquery-example.min.js': ['<%= dirs.web %>js/jquery-example.js', '!<%= dirs.web %>js/jquery-example.min.js']
+				}
+			}
+		},
 
-    watch: {
-      scripts: {
-        files: ['<%= jslint.build.src %>'],
-        tasks: ['jslint','uglify']
-      }
-    }
-  });
+		watch: {
+			options: {
+				livereload: true,
+			},
+			grunt: {
+				options: {
+					livereload: false,
+				},
+				files: ['Gruntfile.js']
+			},
+			scripts: {
+				files: ['<%= jslint.build.src %>'],
+				tasks: ['jslint','uglify']
+			}
+		}
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-jslint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-jslint');
 
-  grunt.registerTask('default', ['jslint','uglify']);
+	grunt.registerTask('default', ['jslint','uglify']);
 
 };
